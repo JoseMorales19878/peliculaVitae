78cfba58b4949830b85aab940a0493fbde8872cc
JoseMorales19878 <128106094+JoseMorales19878@users.noreply.github.com>
2023-04-26 14:18:33 -0400

    modificacion

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],base:'https://josemorales19878.github.io/peliculaVitae'
 })
